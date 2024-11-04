914386d7602aabbea1f1ef0aab3ea0a2e8da8d20
Kasper Van Damme <kasper.van.damme@hotmail.com>
2024-11-04 15:42:44 +0100

    cleanup

--- scripts/app.js
+++ scripts/app.js
@@ -14,9 +14,9 @@
     
                 languages : {
                     de : {
-                        notice: 'Dieser Inhalt wird von einem Dritten gehostet. Durch die Anzeige der externen Inhalte akzeptieren Sie die <a rel="noreferrer noopener" href="https://cloud.google.com/maps-platform/terms" target="_blank">Nutzungsbedingungen</a> von Google Maps.',
+                        notice: 'Dieser Inhalt wird von einem Dritten gehostet. Durch die Anzeige der externen Inhalte akzeptieren Sie die <a rel="noreferrer noopener" href="https://cloud.google.com/maps-platform/terms?hl=de" target="_blank">Nutzungsbedingungen</a> von Google Maps.',
                         loadBtn: 'Laden',
-                        loadAllBtn: "bei jedem Besuch laden"
+                        loadAllBtn: "Bei jedem Besuch laden"
                     }
                 }
             }
